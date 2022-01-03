f93130069be1f4208d3967721d4e39f55b55f36a
Jerry Schrikenberg <j.schrikenberg@eurotransplant.org>
2022-01-03 13:35:34 +0100

    Started exercise 2

--- src/main/java/com/serenitydojo/exceptions/FileLoader.java
+++ src/main/java/com/serenitydojo/exceptions/FileLoader.java
@@ -4,6 +4,7 @@ import io.vavr.control.Try;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
 import java.nio.file.Paths;
 
 public class FileLoader {
@@ -12,8 +13,15 @@ public class FileLoader {
         }
 
     public Boolean fileContainsText(String filename, String expectedText) {
-        String path = "src/main/resources/" + filename;
-        return null;// (Files.readString(Paths.get(path)).contains(expectedText));
+        try {
+            String path = "src/main/resources/" + filename;
+            Files.readString(Paths.get(path)).contains(expectedText);
+            return true;
+        } catch (NoSuchFileException noSuchFile) {
+            return false;
+        } catch (IOException otherExceptions) {
+            return false;
+        }
     }
 
     public Boolean fileHasText(String filename, String expectedText) {
